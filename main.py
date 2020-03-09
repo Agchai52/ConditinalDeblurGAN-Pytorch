1061ef61d2514612d81a6f1e7fbf8451f260b717
Agchai52 <shuangzhang0809@gmail.com>
2020-03-09 16:26:23 -0400

    1*D + 2*G, l_loss_D = 1

--- main.py
+++ main.py
@@ -69,6 +69,10 @@ torch.manual_seed(args.seed)
 with torch.cuda.device(cuda_id):
     print("Current device is: ", torch.cuda.current_device())
     if args.phase == 'train':
+        if not os.path.exists("checkpoint/{}".format(args.dataset_name)):
+            os.makedirs("checkpoint/{}".format(args.dataset_name))
+            os.makedirs("checkpoint/{}/netG".format(args.dataset_name))
+            os.makedirs("checkpoint/{}/netD".format(args.dataset_name))
         if not os.path.exists(args.test_dir):
             os.makedirs(args.test_dir)
         train.train(args)
