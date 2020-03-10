19122af2bc888f95288723fdc17dfe1a84ed387a
Agchai52 <shuangzhang0809@gmail.com>
2020-03-10 19:05:41 -0400

    03102020_02

--- utils.py
+++ utils.py
@@ -37,6 +37,15 @@ def find_latest_model(net_path):
         elif net_path[-1] == 'D':
             return os.path.join(net_path, "D_model_step_{}.pth".format(iter_num))
 
+class LambdaLR():
+    def __init__(self, n_epochs, offset, decay_start_epoch):
+        assert ((n_epochs - decay_start_epoch) > 0), "Decay must start before the training session ends!"
+        self.n_epochs = n_epochs
+        self.offset = offset
+        self.decay_start_epoch = decay_start_epoch
+
+    def step(self, epoch):
+        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch)/(self.n_epochs - self.decay_start_epoch)
 
 def plot_losses():
     loss_record = "loss_record.txt"
