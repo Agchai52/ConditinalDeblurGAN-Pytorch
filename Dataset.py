feff8f1123891c32350a5822e8ff312dc1498de9
Agchai52 <shuangzhang0809@gmail.com>
2020-04-22 13:50:55 -0400

    do not crop during the train

--- Dataset.py
+++ Dataset.py
@@ -18,31 +18,31 @@ class DeblurDataset(Dataset):
         img_name = self.img_path[index][-6:]
         img_name.rstrip()
 
-        if self.is_train:
-            '''
-            Transforms:
-            1. cut black edges
-            2. random crop to (256, 256) patch
-            3. random flip image from left to right
-            4. PIL.Image(H,W,C) to Tensor(C,H,W)
-            5. normalize from [0.0, 1.0] to [-1.0, 1.0]
-            '''
-
-            w = int(img_A.size[0]) - 40
-            h = int(img_A.size[1]) - 40
-
-            img_A = transforms.CenterCrop((h, w))(img_A)
-            img_B = transforms.CenterCrop((h, w))(img_B)
-
-            h1 = int(np.ceil(np.random.uniform(1e-2, h - self.args.fine_size)))
-            w1 = int(np.ceil(np.random.uniform(1e-2, w - self.args.fine_size)))
-
-            img_A = img_A.crop((w1, h1, w1 + self.args.fine_size, h1 + self.args.fine_size))
-            img_B = img_B.crop((w1, h1, w1 + self.args.fine_size, h1 + self.args.fine_size))
-
-            if np.random.random() < 0.5:
-                img_A = img_A.transpose(Image.FLIP_LEFT_RIGHT)
-                img_B = img_B.transpose(Image.FLIP_LEFT_RIGHT)
+        #if self.is_train:
+        #    '''
+        #    Transforms:
+        #    1. cut black edges
+        #    2. random crop to (256, 256) patch
+        #    3. random flip image from left to right
+        #    4. PIL.Image(H,W,C) to Tensor(C,H,W)
+        #    5. normalize from [0.0, 1.0] to [-1.0, 1.0]
+        #    '''
+#
+        #    w = int(img_A.size[0]) - 40
+        #    h = int(img_A.size[1]) - 40
+#
+        #    img_A = transforms.CenterCrop((h, w))(img_A)
+        #    img_B = transforms.CenterCrop((h, w))(img_B)
+#
+        #    h1 = int(np.ceil(np.random.uniform(1e-2, h - self.args.fine_size)))
+        #    w1 = int(np.ceil(np.random.uniform(1e-2, w - self.args.fine_size)))
+#
+        #    img_A = img_A.crop((w1, h1, w1 + self.args.fine_size, h1 + self.args.fine_size))
+        #    img_B = img_B.crop((w1, h1, w1 + self.args.fine_size, h1 + self.args.fine_size))
+#
+        #    if np.random.random() < 0.5:
+        #        img_A = img_A.transpose(Image.FLIP_LEFT_RIGHT)
+        #        img_B = img_B.transpose(Image.FLIP_LEFT_RIGHT)
 
         img_A = self.transform(img_A)
         img_B = self.transform(img_B)
